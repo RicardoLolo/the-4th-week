c597d05319e36c57c915a78540e24ff63db0e245
RicardoLolo <loloringo9999@gmail.com>
2021-12-28 15:25:30 +0700

    the 4th week

---
+++ Threading/src/Count/Count.java
@@ -0,0 +1,28 @@
+package Count;
+
+public class Count implements  Runnable {
+    private Thread myThread;
+    public Count() {
+        Runnable target;
+        myThread = new Thread(this,"my runnable thread");
+        System.out.println("my thread created" + myThread);
+        myThread.start();
+    }
+
+    public Thread getMyThread() {
+        return myThread;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Printing the count" + 1);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("my thread interrupted");
+        }
+        System.out.println("my thread run is over");
+    }
+}
---
+++ Threading/src/RacingCars/Car.java
@@ -0,0 +1,47 @@
+package RacingCars;
+
+import java.util.Random;
+
+import static RacingCars.Main.DISTANCE;
+import static RacingCars.Main.STEP;
+
+public class Car implements Runnable {
+    private static final int DISTANDCE = 300;
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        int runDistance = 0;
+        long startTime = System.currentTimeMillis();
+
+        while (runDistance < DISTANCE){
+            try{
+                int speed = (new Random()).nextInt(30);
+                runDistance += speed;
+                String log = "|";
+                int percentTravel = (runDistance * 100) / DISTANDCE;
+                for (int i = 0; i < DISTANCE; i += STEP) {
+                    if (percentTravel >= i + STEP) {
+                        log += "=";
+                    } else if (percentTravel >= i && percentTravel < i + STEP) {
+                        log += "o";
+                    } else {
+                        log += "-";
+                    }
+                }
+                log += "|";
+                System.out.println("Car" + this.name + ": " + log + " " + Math.min(DISTANCE, runDistance) + "KM");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Car" + this.name + " broken...");
+                break;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Car" + this.name + " Finish in " + (endTime - startTime) / 1000 + "s");
+    }
+}
