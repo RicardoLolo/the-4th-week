969cb25d94157a8c7bcd7f44c8d29f48ac3bd203
RicardoLolo <loloringo9999@gmail.com>
2022-01-04 09:00:29 +0700

    MNT4

---  String & Regex/src/manager.java
+++
@@ -1,9 +0,0 @@
-import BaiTapThem.Student;
-
-public interface manager {
-    void displayAllStudent();
-    void addStudent(Student student);
-    void displayByName(String name);
-    void deleteStudent(String Name);
-    void sortByAvgPoint();
-}
